85331a1244e392f6efb2216982a3f3229c163d7e
Gleb <bakhmetyevgv@gmail.com>
2020-05-16 13:12:40 +0300

    fix drawProjectile method signature

--- graphics/include/GraphicsManager.h
+++ graphics/include/GraphicsManager.h
@@ -19,7 +19,7 @@ public:
 
     void drawObstacle(const std::vector<ObjectInterface> &obstacleData);
 //
-    void drawProjectile(struct ProjectileData projectileData);
+    void drawProjectile(const std::vector<ProjectileInterface> &projectileData);
 
     explicit graphicsManager(Config config);
 
